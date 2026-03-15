0a938324546b9b1a680a76de9a42a1ae38d0e42c
Muralikrishnan Rajendran <mkr.2096@gmail.com>
2026-03-15 17:16:41 -0400

    Serve index.html for root route
    Serve index.html from the public directory.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/api/price", async (req, res) => {
   try {
